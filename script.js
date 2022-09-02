32af671fac83a5cc12d9d1c15b64487b03ca233f
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-09-03 01:32:03 +0400

    parallax on main page

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 "use strict"
 
 window.onload = function(){
-    const parallax = document.querySelector('.parallax__body');
+    const parallax = document.querySelector('.container1');
     if (parallax){
-        const content = document.querySelector('.container1');
-        const sign = document.querySelector('.images-parallax__sign');
+        const content = document.querySelector('.parallax__body');
+        //const sign = document.querySelector('.images-parallax__sign');
+        const bg = document.querySelector('.images-parallax__bg');
+        const table = document.querySelector('.images-parallax__table');
+        const lights = document.querySelector('.images-parallax__lights');
 
-        const forSign = 25;
+        //const forSign = 25;
+        const forBg = 80;
+        const forTable = 20;
+        const forLights = 30;
 
-        const speed = 0.5;
+        const speed = 0.05;
 
         let positionX= 0, positionY = 0;
         let coordXpercent = 0, coordYpercent = 0;
@@ -20,7 +26,10 @@ window.onload = function(){
 
             positionX = positionX + (distX*speed);
             positionY = positionY + (distY*speed);
-            sign.style.cssText = 'transform: translate(${positionX/forSign}%,${positionY/forSign}%);';
+            //sign.style.cssText = 'transform: translate(${positionX/forSign}%,${positionY/forSign}%);';
+            bg.style.cssText = `transform: translate(${positionX/forBg}%,${positionY/forBg}%);`;
+            table.style.cssText = `transform: translate(${positionX/forTable}%,${positionY/forTable}%);`;
+            lights.style.cssText = `transform: translate(${positionX/forLights}%,${positionY/forLights}%);`;
 
             requestAnimationFrame(setMouseParallaxStyle);
         }
