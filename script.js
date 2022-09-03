ad9d7b8699354ed622c19dfb65442ad006f3213e
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-09-04 00:15:23 +0400

    adding new parallax, edite bg img

--- script.js
+++ script.js
@@ -3,18 +3,19 @@
 window.onload = function(){
     const parallax = document.querySelector('.container1');
     if (parallax){
-        const content = document.querySelector('.parallax__body');
+        const content = document.querySelector('.text');
         //const sign = document.querySelector('.images-parallax__sign');
         const bg = document.querySelector('.images-parallax__bg');
         const table = document.querySelector('.images-parallax__table');
         const lights = document.querySelector('.images-parallax__lights');
 
         //const forSign = 25;
-        const forBg = 80;
-        const forTable = 20;
-        const forLights = 30;
+        const forBg = 60;
+        const forTable = 40;
+        const forLights = 35;
+        const forContent = 30;
 
-        const speed = 0.05;
+        const speed = 0.08;
 
         let positionX= 0, positionY = 0;
         let coordXpercent = 0, coordYpercent = 0;
@@ -45,7 +46,28 @@ window.onload = function(){
             coordXpercent = coordX / parallaxWidth*100;
             coordYpercent = coordY / parallaxWidth*100;
 
-        })
+        });
+
+        let theresholdSets=[];
+        for(let i = 0; i<=1.0; i+=0.005){
+            theresholdSets.push(i);
+        }
+        const callback = function (entries, observer){
+            const scrollTopPercent = window.pageYOffset / parallax.offsetHeight * 100;
+            setParallaxItemsStyle(scrollTopPercent);
+        };
+        const observer = new IntersectionObserver(callback, 
+            {thereshold, theresholdSets});
+
+        observer.observe(document.querySelector('.text__header'));
+
+        function setParallaxItemsStyle(scrollTopPercent) {
+
+            content.style.cssText = `transform: translate(0%,-${scrollTopPercent/9}%);`;
+
+            table.ParentElement.style.cssText = `transform: translate(0%,-${scrollTopPercent/6}%);`;
+            lights.ParentElement.style.cssText = `transform: translate(0%,-${scrollTopPercent/3}%);`;
+        }
     }
 }
 
